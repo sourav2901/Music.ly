38594350be7256d10f5fa2aed7d9907584941384
sourav2901 <s.s00554800@gmail.com>
2021-06-01 17:34:33 +0530

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Avicii - Nights",
+    artist: "Avicii",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Maroon-5 – Memories",
+    artist: "Maroon-5",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "PSY - Gangam Style",
+    artist: "PSY",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Maroon-5 - Sugar",
+    artist: "Maroon-5",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Wiz khalifa - See You Again",
+    artist: "Wiz Khalifa ft. Charlie Puth",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Chainsmokers - Closer",
+    artist: "Chainsmokers",
+    img: "music-6",
+    src: "music-6"
+  },
+  // Example:
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // },
+];
